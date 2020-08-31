e169af0f58196f7fd336b1bad2df0e0f7b4e2f0b
monojit18 <datta.monojit@gmail.com>
2020-08-31 12:31:56 +0530

    Update Footer.vue

--- RatingsWeb/Source/src/components/Footer.vue
+++ RatingsWeb/Source/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Kubernetes Service (AKS) workshop
+        Azure RedHat Openshift (ARO) workshop
       </div>
       <div class="col-lg-6">
       </div>
