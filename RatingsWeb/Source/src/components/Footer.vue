9b1026db3aa25c601b1da21eb8d0d3bf141f23ab
monojit18 <datta.monojit@gmail.com>
2020-09-18 13:49:37 +0530

    Update Footer.vue

--- RatingsWeb/Source/src/components/Footer.vue
+++ RatingsWeb/Source/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure RedHat Openshift Workshop
+        Azure RedHat O Workshop
       </div>
       <div class="col-lg-6">
       </div>
