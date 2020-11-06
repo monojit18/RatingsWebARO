fffc06b9d962c9fe5e5fe34b2a588abdd2668a0a
monojit18 <datta.monojit@gmail.com>
2020-11-06 10:56:39 +0530

    footer text changes

--- RatingsWeb/Source/src/components/Footer.vue
+++ RatingsWeb/Source/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure RedHat Openshift Workshop
+        Azure RedHat OSFT Workshop
       </div>
       <div class="col-lg-6">
       </div>
